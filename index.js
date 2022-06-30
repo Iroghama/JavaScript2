1ee2523c2e09b629a1ecb6ea7caf8fd2387b8e8e
Iroghama <99126558+Iroghama@users.noreply.github.com>
2022-06-30 23:27:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
 function operations(){
-    let num1=prompt("enter first number")
-let num2=prompt("enter number 2")
-const operator=prompt("enter operator")
+    let num1=prompt("Enter first number")
+let num2=prompt("Enter second number ")
+const operator=prompt("Enter operator")
 num1=Number(num1)
 num2=Number(num2)
 let sum=""
@@ -26,7 +26,7 @@ let arr=["+","-","/","*"]
         yesNo()
 }
 function yesNo(){
-    let choice=prompt("do yo want to perform another operation(yes OR no)")
+    let choice=prompt("Do you want to perform another operation?(yes OR no)")
     choice=choice.toLowerCase()
     if(choice=="yes"){
         operations()
